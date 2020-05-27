3f89dbd1ca0aec8e02d3295b7d1f8a096c887ea1
Jasmine Huang <tinjasminee@gmail.com>
2020-05-27 13:01:42 +0800

    finish logic

--- Gemfile
+++ Gemfile
@@ -27,6 +27,8 @@ gem 'bootsnap', '>= 1.4.2', require: false
 
 gem 'simple_form'
 
+gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+10.times do
+  r = Restaurant.create!(name: Faker::Hipster.word,
+                         address: Faker::Address.street_address,
+                         category: ["chinese", "italian", "japanese", "french", "belgian"].sample,
+                         phone_number: Faker::Number.leading_zero_number(digits: 10))
+  rand(1..5).times do
+    Review.create!(content: Faker::TvShows::RickAndMorty.quote,
+                   rating: rand(0..5),
+                   restaurant_id: r.id )
+  end
+end
